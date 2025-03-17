2d58a69707183852162aada6e0639aab6d10752f
Cameron Hu <camcamhu@gmail.com>
2025-03-17 15:34:28 -0700

    Initial assignToBoxes - CPU only
    * boxes and particle_idx allocated on CPU
    
    * boxes data structure
    
    * gpu allocation copy working
    
    * init box assignment, segfault
    
    * assignToBoxes debug printing
    
    * assignToBoxes cpu implementation, no copy

--- gpu.cu
+++ gpu.cu
@@ -1,10 +1,44 @@
 #include "common.h"
 #include <cuda.h>
+#include <stdio.h>
 
 #define NUM_THREADS 256
+#define INDEX(row, col) ((row) * numBoxes1D + (col))
 
 // Put any static global variables here that you will use throughout the simulation.
 int blks;
+double boxSize1D = cutoff;
+int numBoxes1D;
+int totalBoxes;
+
+
+// ============ Array pointers for boxes and particle_idx ============
+
+// CPU arrays
+int* boxCounts;
+int* prefixSums;
+int* particle_ids;
+int* boxes;
+
+// GPU arrays
+int* gpu_boxCounts;
+int* gpu_prefixSums;
+int* gpu_particle_ids;
+int* gpu_boxes;
+
+// =================
+// Helper Functions
+// =================
+
+/**
+* Helper function to calculate the box index of a given particle
+*/
+int findBox(const particle_t& p) {
+    int col = floor(p.x / boxSize1D);
+    int row = floor(p.y / boxSize1D);
+    return INDEX(row, col);
+}
+
 
 __device__ void apply_force_gpu(particle_t& particle, particle_t& neighbor) {
     double dx = neighbor.x - particle.x;
@@ -65,18 +99,96 @@ __global__ void move_gpu(particle_t* particles, int num_parts, double size) {
     }
 }
 
+void assignToBoxes(particle_t* parts, int num_parts) {
+    setbuf(stdout, NULL);
+    printf("Inside assignToBoxes\n");
+
+    // Copy from parts (gpu_parts) to cpu_parts
+    particle_t* cpu_parts = new particle_t[num_parts];
+    cudaMemcpy(cpu_parts, parts, num_parts * sizeof(particle_t), cudaMemcpyDeviceToHost);
+    size_t actual_size = 0;
+    for (int i = 0; i < num_parts; ++i) {
+        actual_size += sizeof(cpu_parts[i]);
+    }
+    printf("Actual size of cpu_parts: %lu\n", actual_size);
+    printf("Num particles in cpu_parts: %lu\n", actual_size / sizeof(particle_t));
+    
+    // First pass: count particles in each box
+    for (int i = 0; i < num_parts; ++i) {
+        printf("cur parts idx: %i\n", i);
+        int boxIndex = findBox(cpu_parts[i]);
+        printf("boxIndex: %i\n", boxIndex);
+        boxCounts[boxIndex]++;
+    }
+
+    printf("Fin counting particles per box\n");
+
+    // Compute starting index for each box in particle_idx
+    int prefixSum = 0;
+    for (int boxIndex = 0; boxIndex <= totalBoxes; ++boxIndex) {
+        prefixSums[boxIndex] = prefixSum;
+        prefixSum += boxCounts[boxIndex];
+    }
+    printf("Last value of prefix sums should be num_parts. prefixSums[-1]: %i. num_parts: %i\n", prefixSums[totalBoxes], num_parts);
+
+    printf("Fin calc starting particle_idx index for each box's first part\n");
+
+    // Reset box counts for use in the second pass
+    memset(boxCounts, 0, totalBoxes * sizeof(int));
+
+    // Second pass: assign particles to particle_idx and update boxes
+    for (int i = 0; i < num_parts; ++i) {
+        int boxIndex = findBox(cpu_parts[i]);
+        int pos = prefixSums[boxIndex] + boxCounts[boxIndex];
+        particle_ids[pos] = i;
+        boxCounts[boxIndex]++;
+    }
+    printf("Fin second pass to assign particles `parts` index to particle_ids in proper box order.\n");
+
+    // Update boxes array: -1 if box has no particles
+    for (int i = 0; i < totalBoxes; ++i) {
+        boxes[i] = (boxCounts[i] > 0) ? prefixSums[i] : -1;
+    }
+    printf("Updating `boxes` array with starting indices if box has particles.\n");
+}
+
 void init_simulation(particle_t* parts, int num_parts, double size) {
     // You can use this space to initialize data objects that you may need
     // This function will be called once before the algorithm begins
     // parts live in GPU memory
     // Do not do any particle simulation here
+    setbuf(stdout, NULL);
 
+    // Assign global variables
     blks = (num_parts + NUM_THREADS - 1) / NUM_THREADS;
+    numBoxes1D = ceil(size / boxSize1D);
+    totalBoxes = numBoxes1D * numBoxes1D;
+
+    // Allocate memory for CPU-side arrays
+    boxCounts = new int[totalBoxes]();
+    prefixSums = new int[totalBoxes];
+    particle_ids = new int[num_parts];
+    boxes = new int[totalBoxes];
+
+    // Allocate memory for GPU-side arrays and copy from CPU-side arrays
+    cudaMalloc((void**)&gpu_boxCounts, totalBoxes * sizeof(int));
+    cudaMemset(gpu_boxCounts, 0, totalBoxes * sizeof(int));
+    // cudaMemcpy(gpu_boxCounts, boxCounts, totalBoxes * sizeof(int), cudaMemcpyHostToDevice);
+
+    cudaMalloc((void**)&gpu_prefixSums, totalBoxes * sizeof(int));
+
+    cudaMalloc((void**)&gpu_particle_ids, num_parts * sizeof(int));
+
+    cudaMalloc((void**)&gpu_boxes, totalBoxes * sizeof(int));
+    printf("Numboxes1d: %i. totalBoxes: %i\n", numBoxes1D, totalBoxes);
 }
 
 void simulate_one_step(particle_t* parts, int num_parts, double size) {
     // parts live in GPU memory
     // Rewrite this function
+
+    // Assign all particles to boxes
+    assignToBoxes(parts, num_parts);
 
     // Compute forces
     compute_forces_gpu<<<blks, NUM_THREADS>>>(parts, num_parts);
